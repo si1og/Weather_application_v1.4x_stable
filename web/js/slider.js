85677788783cd5d2315d062d7dd905c9c3768a74
unknown <sialogacc@gmail.com>
2022-05-28 21:52:49 +0300

    Weather_app - v1.4.0.0.03

---
+++ web/js/slider.js
@@ -0,0 +1,28 @@
+
+var swiper_hourly_foresast = new Swiper(".hourly-forecast__slider-block", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.hourly-forecast__right-button',
+        prevEl: '.hourly-forecast__left-button',
+    }
+});
+
+var seven_day_forecast_slider = new Swiper(".seven-day-forecast-short__content", {
+    slidesPerView: 7,
+    spaceBetween: 10,
+    breakpoints: {
+        1000: {
+            slidesPerView: 7
+        },
+        1200: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 6
+        },
+        1440: {
+            slidesPerView: 7
+        }
+    }
+});
